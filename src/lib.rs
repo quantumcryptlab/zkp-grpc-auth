0129c73bbd5997667ab356f7817b8278aa9d3981
Xudong ZHAO <xudong.zhao@protonmail.com>
2025-06-02 18:47:52 +0200

    Add random number generator

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-use num_bigint::BigUint;
+use num_bigint::{BigUint, RandBigInt};
+use rand;
 
-/// alpha^x mod p 
+/// alpha^x mod p
 /// output = n ^ exp mod p
 pub fn exponentiate(n: &BigUint, exponent: &BigUint, modulus: &BigUint) -> BigUint {
     n.modpow(exponent, modulus)
@@ -21,15 +22,21 @@ pub fn verify(
     r2: &BigUint,
     y1: &BigUint,
     y2: &BigUint,
-    alpha: &BigUint, 
+    alpha: &BigUint,
     beta: &BigUint,
     c: &BigUint,
     s: &BigUint,
     p: &BigUint,
 ) -> bool {
-    let cond1 = *r1 == (alpha.modpow(s, p) * y1.modpow(c,p)).modpow(&BigUint::from(1u32),&p);
-    let cond2 = *r2 == (beta.modpow(s, p) * y2.modpow(c, p)).modpow(&BigUint::from(1u32),&p);
+    let cond1 = *r1 == (alpha.modpow(s, p) * y1.modpow(c, p)).modpow(&BigUint::from(1u32), &p);
+    let cond2 = *r2 == (beta.modpow(s, p) * y2.modpow(c, p)).modpow(&BigUint::from(1u32), &p);
     cond1 && cond2
+}
+
+pub fn generate_random_below(bound: &BigUint) -> BigUint {
+    let mut rng = rand::thread_rng();
+
+    rng.gen_biguint_below(bound)
 }
 
 #[cfg(test)]
@@ -56,27 +63,42 @@ mod test {
         let r1 = exponentiate(&alpha, &k, &p);
         let r2 = exponentiate(&beta, &k, &p);
         assert_eq!(r1, BigUint::from(8u32));
-        assert_eq!(r2,BigUint::from(4u32));
+        assert_eq!(r2, BigUint::from(4u32));
 
-        let s = solve(&k, &c, &x ,&q);
+        let s = solve(&k, &c, &x, &q);
         assert_eq!(s, BigUint::from(5u32));
 
         let result = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s, &p);
         assert!(result);
 
-         // fake secret 
-         let x_fake = BigUint::from(7u32);
-         let s_fake = solve(&k, &c, &x_fake ,&q);
-         let result = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s_fake, &p);
+        // fake secret
+        let x_fake = BigUint::from(7u32);
+        let s_fake = solve(&k, &c, &x_fake, &q);
+        let result = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s_fake, &p);
         assert!(!result)
-
-
-
-
-
-
-
     }
 
-}
+    #[test]
+    fn test_toy_example_with_rand() {
+        let alpha = BigUint::from(4u32);
+        let beta = BigUint::from(9u32);
+        let p = BigUint::from(23u32);
+        let q = BigUint::from(11u32);
 
+        let x = BigUint::from(6u32);
+        let k = generate_random_below(&q);
+
+        let c = generate_random_below(&q);
+
+        let y1 = exponentiate(&alpha, &x, &p);
+        let y2 = exponentiate(&beta, &x, &p);
+
+        let r1 = exponentiate(&alpha, &k, &p);
+        let r2 = exponentiate(&beta, &k, &p);
+
+        let s = solve(&k, &c, &x, &q);
+
+        let result = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s, &p);
+        assert!(result);
+    }
+}
